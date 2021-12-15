74a7df7b97cd69c4993b6032cee005436661a3da
Mukti Singh <{ID}+{username}@users.noreply.github.com>
2021-12-16 01:18:24 +0530

    Styles added

--- src/Coin.js
+++ src/Coin.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import './Coin.css'
 
-const Coin = ({name, image, symbol, price, volume, priceChange}) => {
+const Coin = ({name, image, symbol, price, volume, priceChange, marketcap}) => {
     return (
         <div className='coin-cointainer'>
             <div className='coin-row'>
@@ -14,7 +15,10 @@ const Coin = ({name, image, symbol, price, volume, priceChange}) => {
                     <p className='coin-volume'>${volume.toLocaleString()}</p>
                     {priceChange < 0 ? (
                         <p className='coin-percent red'>{priceChange.toFixed(2)}%</p>
-                    ):<p className='coin-percent green'>{priceChange.toFixed(2)}%</p>}
+                    ):(<p className='coin-percent green'>{priceChange.toFixed(2)}%</p>)}
+                    <p className='coin-marketcap'>
+                        Mkt Cap: ${marketcap.toLocaleString()}
+                    </p>
                  </div>
             </div>
         </div>
